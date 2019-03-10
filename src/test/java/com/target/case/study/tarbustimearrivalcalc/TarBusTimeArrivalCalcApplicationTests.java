f3672ded2f968eeab7a7d7f839adab42e311d6ec
vamshi <atvamshi@gmail.com>
2019-03-10 16:09:07 -0500

    First commit

---
+++ src/test/java/com/target/case/study/tarbustimearrivalcalc/TarBusTimeArrivalCalcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.target.case.study.tarbustimearrivalcalc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TarBusTimeArrivalCalcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
